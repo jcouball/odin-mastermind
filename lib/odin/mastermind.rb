44a54bcca81a3b6f49936b24b9f98658fb747472
James Couball <jcouball@yahoo.com>
2025-09-20 09:28:58 -0700

    Generated project from bundle gem

---
+++ lib/odin/mastermind.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "mastermind/version"
+
+module Odin
+  module Mastermind
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
